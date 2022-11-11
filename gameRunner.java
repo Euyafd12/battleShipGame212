2fac46256f491ea9717df363d55b5a4dfccaef5f
Euyafd12 <euyafd1234@gmail.com>
2022-11-10 21:32:39 -0600

    We can start now

--- gameRunner.java
+++ gameRunner.java
@@ -1,9 +1,6 @@
 public class gameRunner {
 
     public static void main(String[] args) {
-
-        System.out.println("This will be our gameRunner");
-        System.out.println("this is testing our commit");
-        System.out.println("this should be a branch test");
+        //we code seperate from code base
     }
 }
