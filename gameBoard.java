0f0413b9bc30624a227c93b327e32d3176bef578
Eyad Aboelnaga <S929030@cfisd.loc>
2022-11-15 13:37:57 -0600

    asdasdsd

--- gameBoard.java
+++ gameBoard.java
@@ -1,4 +1,34 @@
 public class gameBoard {
+    private char[][] board;
 
-    
+    public gameBoard() {
+
+        board = new char[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                board[i][j] = '0';
+            }
+        }
+    }
+
+    public String boardOneLine() {
+
+        String ret = "";
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                if (j != 9) {
+                    ret += board[i][j] + ", ";
+                }
+                else {
+                    ret += board[i][j];
+                }
+            }
+        }
+
+        return ret;
+    }
+
 }
