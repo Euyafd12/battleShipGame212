76d6bba3bcb83ced8976189f5049f605f84d7f49
Leo2954 <leospillane2020@outlook.com>
2022-11-16 13:43:07 -0600

    cock

--- Player.java
+++ Player.java
@@ -1,2 +1,30 @@
-public class Player {
+public class Player extends gameBoard{
+
+    private String name;
+    private char[][] board;
+
+    public Player(){
+        board=super.gameBoard();
+        name ="";
+
+    }
+
+    public String boardOneLine() {
+
+        String ret = "";
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                if (j != 9) {
+                    ret += board[i][j] + ", ";
+                }
+                else {
+                    ret += board[i][j];
+                }
+            }
+        }
+
+        return ret;
+    }
 }
