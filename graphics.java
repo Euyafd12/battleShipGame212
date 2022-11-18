cfd92c293495b1bc7bb290672b0cf204c0b52156
Eyad Aboelnaga <S929030@cfisd.loc>
2022-11-18 13:33:35 -0600

    asdas

--- graphics.java
+++ graphics.java
@@ -6,47 +6,23 @@ import java.util.EventListener;
 import java.util.HashMap;
 import java.util.Map;
 
-public class graphics extends JPanel{
+public class graphics extends JPanel implements MouseListener{
     private final Image imageBoard, imageBackground, imageICN;
-
+    private int x, y;
 
     public graphics() {
 
-        int x = MouseInfo.getPointerInfo().getLocation().x;
-        int y = MouseInfo.getPointerInfo().getLocation().y;
-
-        //JLabel label = new JLabel("Pointing at x - " + x + " & at y - " + y);
-        //this.add(label);
-
-
-        //imageBoard = new ImageIcon("boardV1.png").getImage();
-        //imageBoard = new ImageIcon("boardV2.png").getImage();
         imageBoard = new ImageIcon("boardV3.png").getImage();
-        //imageBoard = new ImageIcon("boardV4.png").getImage();
-
         imageBackground = new ImageIcon("world-of-warships.jpg").getImage();
         imageICN = new ImageIcon("icon.png").getImage();
+    }
 
-        /*
-        try {
-
-            while (true) {
-
-                x = MouseInfo.getPointerInfo().getLocation().x;
-                y = MouseInfo.getPointerInfo().getLocation().y;
-
-                label.setText("Pointing at x - " + x + " & at y - " + y);
-
-                this.revalidate();
-                this.repaint();
-                Thread.sleep(25);
-
-            }
-        }
-        catch (Exception ignored) {
-
-        }
-        */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        x = MouseInfo.getPointerInfo().getLocation().x;
+        y = MouseInfo.getPointerInfo().getLocation().y;
+        System.out.print("X - coordinate: " + x + " & Y - coordinate: " + y + "  -------------------------- ") ;
+        repaint();
     }
 
     public void display() {
@@ -55,9 +31,11 @@ public class graphics extends JPanel{
         frame.setIconImage(imageICN);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
-        frame.pack();
+        addMouseListener(this);
+
 
         //Dimension monitor = Toolkit.getDefaultToolkit().getScreenSize();
+
         frame.setSize(1920, 1080);
         frame.setVisible(true);
     }
@@ -67,6 +45,7 @@ public class graphics extends JPanel{
 
         Graphics2D g2d = (Graphics2D) window;
 
+
         g2d.drawImage(imageBackground, 0, 0, 1920, 1080, null);
         g2d.drawImage(imageBoard, 900, 50, 900, 900, null);
 
@@ -74,37 +53,14 @@ public class graphics extends JPanel{
         g2d.fillRect(50, 75, 350, 900);
         g2d.setColor(Color.BLACK);
         g2d.drawRect(50, 75, 350, 900);
-        
-        /*
-        while (true) {
 
-            if (MouseEvent.BUTTON1 == 1) {
-                coordinateClick();
-            }
+        System.out.println(coordinateClick());
 
-
-        }
-        */
     }
-
-    /*
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-        int x = MouseInfo.getPointerInfo().getLocation().x;
-        int y = MouseInfo.getPointerInfo().getLocation().y;
-
-        coordinateClick(x, y);
-    }
-
-
 
     public String coordinateClick() {
 
         String ret = "";
-
-        int x = MouseInfo.getPointerInfo().getLocation().x;
-        int y = MouseInfo.getPointerInfo().getLocation().y;
 
         if (x >= 970 && x <= 1870 && y >= 165 && y <= 1065) {
 
@@ -117,21 +73,50 @@ public class graphics extends JPanel{
                 letter.put(i + 1, "" + (char) ('A' + i));
             }
 
-            ret += letter.get(((y - baseY) / 90) + 1) + " " + (((x - baseX) / 90) + 1);
-            System.out.println(ret);
+            ret += letter.get(((y - baseY) / 90) + 1) + "" + (((x - baseX) / 90) + 1);
             return ret;
         }
         else {
-            System.out.println("Error");
             return "Error";
         }
     }
 
-    */
 
     public static void main(String[] args) {
 
         new graphics().display();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //Dead method
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //Dead method
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //Dead method
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //Dead method
+    }
 }
